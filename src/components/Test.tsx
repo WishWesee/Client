508b7914134ede35569b63c5d29e99d8f3f027d7
김홍민 <khhm3787@gmail.com>
2024-12-26 17:26:44 +0900

    버튼 컴포넌트 구성

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,17 @@
+import SlideButton from "./Btn_Boolean";
+import Button_Bottom_Next from "./Btn_Bottom_Next";
+import Btn_Modal_Caution from "./Btn_Modal_Caution";
+import RadioButton from "./Btn_Radio";
+
 const Tsta = () => {
   return <div>
     <svg>
       <use xlinkHref="src/assets/logo.svg#HeaderLogo"></use>
     </svg>
+    <Button_Bottom_Next/>
+    <SlideButton/>
+    <Btn_Modal_Caution/>
+    <RadioButton/>
   </div>;
 };
 
