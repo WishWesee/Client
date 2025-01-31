cb1110683405089c614a525c6aa7af02de442d60
김홍민 <khhm3787@gmail.com>
2025-02-01 00:38:27 +0900

    구글로그인 세팅 변경

--- src/api/login/profile.ts
+++ src/api/login/profile.ts
@@ -1,8 +1,13 @@
-import { api } from "..";
+import axios from "axios";
 
 export const FetchMyInfo = async() => {
   try {
-    const response = await api.get("/api/v1/user");
+    const response = await axios.get("https://wishwesee.n-e.kr/api/v1/user", {
+      headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJraGhtMzc4N0BnbWFpbC5jb20iLCJpYXQiOjE3MzgzMzYwNjksImV4cCI6MTczODMzOTY2OX0.Be5D-fjkTy2zaqKemEji1L18Rrwy7hYux-PgZImD_4NAc7tfaR2vFoYMrM5EfTAMHblMeaGmc1ZzPKKWFp4nnw",
+        "Content-Type": "application/json",
+      },
+    });
     console.log("API Response:", response.data);
     return response.data.information;
   } catch (error) {
