76eda6abb515adff821756811ee0469d65586331
rlotr02 <0314kelee@naver.com>
2025-02-02 16:12:54 +0900

    fix: 홈화면 로그인 조건 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import FeaturesGuide from "@/components/home/FeaturesGuide";
 import HomeLogin from "@/components/home/HomeLogin";
 import UsageGuide from "@/components/home/UsageGuide";
+import { useAuthStore } from "@/store/useAuthStore";
 
 const Home = () => {
-  const isLogin = !!localStorage.getItem("authToken");
+  const { isLogin } = useAuthStore();
 
   return (
     <>
