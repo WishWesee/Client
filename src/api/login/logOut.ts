e2bb4a8848bced4875c407ca5c01fba19d229186
김홍민 <khhm3787@gmail.com>
2025-02-04 19:43:25 +0900

    내 아이콘 위치 조정 및 로그아웃/탈퇴 수정

--- src/api/login/logOut.ts
+++ src/api/login/logOut.ts
@@ -6,11 +6,10 @@ export const Logout = async (): Promise<void> => {
 
     document.cookie.split(";").forEach((cookie) => {
       const [name] = cookie.split("=");
-      document.cookie = `${name.trim()}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${window.location.hostname}`;
+      document.cookie = `${name.trim()}=; max-age=-1; path=/; domain=.wishwesee.n-e.kr`;
     });
-    
-    console.log("로그아웃 성공, 쿠키 삭제 완료");
 
+    console.log("로그아웃 성공, 쿠키 삭제 완료");
     window.location.href = "/";
   } catch (error) {
     console.error("로그아웃 요청 실패:", error);
