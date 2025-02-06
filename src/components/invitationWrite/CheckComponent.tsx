351bfeb0ee7bdc2117a7e67fe801b22b74fc8334
si14444 <si14444@naver.com>
2025-02-06 19:40:05 +0900

    fix: bottom 수정

--- src/components/invitationWrite/CheckComponent.tsx
+++ src/components/invitationWrite/CheckComponent.tsx
@@ -1,32 +1,17 @@
 import CheckContentWrap from "@/components/invite/CheckContentWrap";
-import InviteBottom from "@/components/invite/InviteBottom";
 import TotheTopBtn from "@/components/invite/TotheTopBtn";
 import useWMediaQuery from "@/hooks/useWMediaQuery";
-import { usePostInvitation } from "@/hooks/write/usePostInvitation";
-import useInvitationStore from "@/store/invitation";
 import { TInvitationReq } from "@/types/invite";
 import * as S from "@styles/invite/InvitationDetailPageStyle";
 import { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import InvitationWriteHeader from "./InvitationWriteHeader";
 
 type Props = {
   data: TInvitationReq;
-  isCheck: boolean;
-  onLeftBtnClick: () => void;
-  onRightBtnClick: () => void;
 };
 
-const CheckComponent = ({
-  data,
-  isCheck,
-  onLeftBtnClick,
-  onRightBtnClick,
-}: Props) => {
+const CheckComponent = ({ data }: Props) => {
   const { isMobile } = useWMediaQuery();
-  const navigate = useNavigate();
-  const { mutate: postInvitation } = usePostInvitation();
-  const { resetInvitation } = useInvitationStore();
+
   //애니메이션 스크롤
   const [scrollY, setScrollY] = useState(0);
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -62,48 +47,16 @@ const CheckComponent = ({
     }
   }, [animationStarted]);
 
-  //완성된 초대장 저장
-  const handleSaveInvite = () => {
-    const formData = new FormData();
-    formData.append(
-      "invitation",
-      new Blob([JSON.stringify(data.invitation)], { type: "application/json" })
-    );
-    if (data.cardImage) {
-      formData.append("cardImage", data.cardImage);
-    }
-
-    if (data.photoImages && data.photoImages.length > 0) {
-      Array.from(data.photoImages).forEach((file) => {
-        formData.append("photoImages", file);
-      });
-    }
-
-    postInvitation(formData, {
-      onSuccess: (response) => {
-        //저장 후 결과로 받은 id값
-        resetInvitation();
-        const id = response.invitationId;
-        navigate(`/invites/${id}`, {
-          state: { isDone: true },
-        });
-      },
-      onError: (error) => {
-        console.error("등록 실패:", error);
-      },
-    });
-  };
-
   return (
     <S.Container $isHeader={true}>
       {data && (
         <>
-          <InvitationWriteHeader
+          {/* <InvitationWriteHeader
             backText="이전"
             buttonType="저장"
             onLeftBtnClick={onLeftBtnClick}
             onRightBtnClick={onRightBtnClick}
-          />
+          /> */}
           {isMobile && (
             <S.FadeInImage
               src={
@@ -124,13 +77,13 @@ const CheckComponent = ({
               <S.Overlay style={{ opacity: Math.max(1 - scrollY / 300, 0) }} />
             )}
             <CheckContentWrap data={data} />
-            <InviteBottom
+            {/* <InviteBottom
               btnText="완료"
               onBtnClick={handleSaveInvite}
               isDisabled={false}
               isCheck={isCheck}
               onCheckClick={() => console.log(isCheck)}
-            />
+            /> */}
           </S.BodyWrap>
         </>
       )}
