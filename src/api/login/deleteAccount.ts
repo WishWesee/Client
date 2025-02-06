b8999b998cc2607fee33827c31b92940b47ccac7
김홍민 <khhm3787@gmail.com>
2025-02-06 23:15:21 +0900

    회원탈퇴 오류 수정

--- src/api/login/deleteAccount.ts
+++ src/api/login/deleteAccount.ts
@@ -6,11 +6,11 @@ export const DeleteAccount = async (): Promise<void> => {
 
       document.cookie.split(";").forEach((cookie) => {
         const [name] = cookie.split("=");
-        document.cookie = `${name.trim()}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=.wishwesee.n-e.kr`;
+        document.cookie = `${name.trim()}=; max-age=-1; path=/; domain=.wishwesee.n-e.kr`;
       });
   
       console.log("쿠키 삭제 완료");
-      console.log("탈퇴됨");
+      window.location.href = "/";
     } catch (error) {
       console.error("탈퇴 요청 실패:", error);
       throw error;
